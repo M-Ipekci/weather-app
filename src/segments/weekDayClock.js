50de15b842abbf242b290be6b85d9a75defe1c18
Mustafa I <mripekci@gmail.com>
2024-04-05 01:50:05 -0700

    weekdayclock and style

---
+++ src/segments/weekDayClock.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import 'src/styles/wdc.css';
+
+function WeekDayClock() {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
+
+  const dayOfWeek = dateTime.toLocaleDateString('en-US', { weekday: 'long' });
+  const currentTime = dateTime.toLocaleTimeString('en-US');
+
+  return (
+    <div>
+      <h2>{dayOfWeek}</h2>
+      <p>{currentTime}</p>
+    </div>
+  );
+}
+
+export default WeekDayClock;
